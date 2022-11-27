11b68a1d07d4f071126c4099eac2b2e07340c50c
Eduardo Botelho de Castro Pinto <a45480@alunos.ipb.pt>
2022-11-27 02:12:57 +0000

    feature: Home(estatico)

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/Home';
+
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
+  return (
+    <NavigationContainer>
+      <Tabs.Navigator
+        screenOptions={{ headerShown: false, tabBarStyle: { display: 'none' } }}
+      >
+        <Tabs.Screen name="Home" component={Home} />
+      </Tabs.Navigator>
+    </NavigationContainer>
+  );
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ screens/Home.js
@@ -0,0 +1,179 @@
+import { Button } from '@rneui/base';
+import { FlatList } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <FlatList
+        ListHeaderComponent={PageContent}
+        contentContainerStyle={{ minHeight: '100%' }}
+        ListHeaderComponentStyle={{ flex: 1 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+function PageContent() {
+  return (
+    <View style={{ justifyContent: 'center', flex: 1 }}>
+      <View style={{ padding: 48 }}>
+        <Text style={{ textAlign: 'center', fontSize: 24 }}>Eduardo</Text>
+        <Text style={{ textAlign: 'center', fontSize: 24 }}>Pontos: 0</Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 24,
+          paddingBottom: 12,
+        }}
+      >
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'9'}
+          color="green"
+        />
+        <Button
+          containerStyle={{ flex: 1, paddingHorizontal: 8 }}
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'Inglês'}
+          color="cornflowerblue"
+        />
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'L'}
+          color="green"
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 24,
+          paddingBottom: 12,
+        }}
+      >
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'9'}
+          color="green"
+        />
+        <Button
+          containerStyle={{ flex: 1, paddingHorizontal: 8 }}
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'HTML'}
+          color="cornflowerblue"
+        />
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'L'}
+          color="green"
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 24,
+          paddingBottom: 12,
+        }}
+      >
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'9'}
+          color="green"
+        />
+        <Button
+          containerStyle={{ flex: 1, paddingHorizontal: 8 }}
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'CSS'}
+          color="cornflowerblue"
+        />
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'L'}
+          color="green"
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 24,
+          paddingBottom: 12,
+        }}
+      >
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'9'}
+          color="green"
+        />
+        <Button
+          containerStyle={{ flex: 1, paddingHorizontal: 8 }}
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'JavaScript'}
+          color="cornflowerblue"
+        />
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'L'}
+          color="green"
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          paddingHorizontal: 24,
+          paddingBottom: 48,
+        }}
+      >
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'9'}
+          color="green"
+        />
+        <Button
+          containerStyle={{ flex: 1, paddingHorizontal: 8 }}
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'Programação'}
+          color="cornflowerblue"
+        />
+        <Button
+          buttonStyle={{ width: 48, height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title={'L'}
+          color="green"
+        />
+      </View>
+      <View style={{ paddingHorizontal: 24, paddingBottom: 48 }}>
+        <Button
+          containerStyle={{ paddingBottom: 8 }}
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title="START"
+          color="indigo"
+        />
+        <Button
+          buttonStyle={{ height: 48 }}
+          titleStyle={{ fontSize: 20 }}
+          title="STATS"
+          color="blueviolet"
+        />
+      </View>
+    </View>
+  );
+}
+
+const page_styles = StyleSheet.create({});
